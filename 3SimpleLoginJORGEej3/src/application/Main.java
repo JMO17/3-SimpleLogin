a5bb349f888f299ddafb0065405a02cf9d0bbf68
jorge <jorge@XJO-Khalessi001.mshome.net>
2018-12-06 22:55:56 +0100

    fin

---
+++ 3SimpleLoginJORGEej3/src/application/Main.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) throws IOException {
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("SignIn.fxml"));
+		AnchorPane mypane = loader.load();
+
+		// Añade la escena al stage y la titula
+		stage.setTitle("Login");
+		stage.setScene(new Scene(mypane));
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
